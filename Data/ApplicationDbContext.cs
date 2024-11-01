3d90ca134054ee1cb98db7af2d05b0f7b78c195a
Rabaka07 <rabaquimruan07@gmail.com>
2024-11-01 16:09:55 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APIBotas.Models;
+
+namespace APIBotas.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<APIBotas.Models.Cliente> Cliente { get; set; } = default!;
+        public DbSet<APIBotas.Models.Compra> Compra { get; set; } = default!;
+        public DbSet<APIBotas.Models.Doce> Doce { get; set; } = default!;
+        public DbSet<APIBotas.Models.Produto> Produto { get; set; } = default!;
+    }
+}
---
+++ Models/Compra.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIBotas.Models
+{
+    public class Compra
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime DataCompra { get; set; }
+
+
+        [ForeignKey("DoceId")]
+        public Guid DoceId { get; set; }
+        [ForeignKey("ClienteId")]
+        public Guid ClienteId { get; set; }
+
+        public Doce? Doce { get; set; }
+        public Cliente? Cliente { get; set; }
+    }
+}
